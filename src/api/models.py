8cbab3cbdca71ecf32e7b2a65508caee1b0aa57a
chrisgonvill <124818047+chrisgonvill@users.noreply.github.com>
2023-12-22 21:49:25 +0000

    fixed error in previews branch

--- src/api/models.py
+++ src/api/models.py
@@ -9,18 +9,17 @@ class User(db.Model):
     last_name = db.Column(db.String(120), unique=False, nullable=False)
     date_of_birth = db.Column(db.String(120), unique=False, nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    pathologies = db.Column(db.String(120), unique=True, nullable=False)
+    pathologies = db.Column(db.String(120), unique=False, nullable=True)
     password = db.Column(db.String(80), unique=False, nullable=False)
     id_routine = db.relationship('Routines', backref='user', lazy=True)
     id_diets = db.relationship('Diets', backref='user', lazy=True)
     id_trainer = db.relationship('Trainer', backref='user', lazy=True)
     create_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
     
-
-def __repr__(self):
+    def __repr__(self):
         return f'<User {self.id}>'
 
-def serialize(self):
+    def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
@@ -29,8 +28,11 @@ def serialize(self):
             "date_of_birth": self.date_of_birth,
             "routine": self.routine,
             "diet": self.diet,
-            "pathologies": self.pathologies           
-        }
+            "pathologies": self.pathologies,
+            "id_routine": self.get_routine_serialized(),
+            "id_diets": self.get_diets_serialized(),
+            "id_trainer": self.get_trainer_serialized()
+           }
     
 
 class Admins(db.Model):
@@ -38,15 +40,14 @@ class Admins(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     
-def __repr__(self):
+    def __repr__(self):
         return f'<Admins {self.id}>'
 
-def serialize(self):
+    def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
-            
-        }
+            }
 
 
 class Trainer(db.Model):
@@ -60,17 +61,19 @@ class Trainer(db.Model):
     id_routine = db.relationship('Routines', backref='trainer', lazy=True)
     create_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
     
-def __repr__(self):
+    def __repr__(self):
         return f'<Trainer {self.id}>'
 
-def serialize(self):
+    def serialize(self):
         return {
             "id": self.id,
             "first_name": self.first_name,
             "last_name": self.last_name,
             "email": self.email,
-            
-        }
+            "id_user": self.get_user_serialized(),
+            "id_diets": self.get_diets_serialized(),
+            "id_routine": self.get_routine_serialized()
+            }
     
     
 
@@ -83,18 +86,18 @@ class Routines(db.Model):
     id_user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     id_trainer = db.Column(db.Integer, db.ForeignKey('trainer.id'), nullable=False)
     
-def __repr__(self):
+    def __repr__(self):
         return f'<Routines {self.id}>'
 
-def serialize(self):
+    def serialize(self):
         return {
             "id": self.id,
             "Chest": self.Chest,
             "shoulders": self.shoulders,
             "arms": self.arms,
             "legs": self.legs,
-            "id_user": self.get_user_serialized(),  # Serializar usuario si existe
-            "id_trainer": self.get_trainer_serialized()  # Serializar entrenador si existe
+            "id_user": self.get_user_serialized(),  
+            "id_trainer": self.get_trainer_serialized()  
         }
 
 
@@ -107,11 +110,11 @@ class Diets(db.Model):
     supper = db.Column(db.String(120), unique=False, nullable=False)
     id_trainer = db.Column(db.Integer, db.ForeignKey('trainer.id'), nullable=False)
     id_user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-def __repr__(self):
+    
+    def __repr__(self):
         return f'<Diets {self.id}>'
 
-def serialize(self):
+    def serialize(self):
         return {
             "id": self.id,
             "breakfast": self.breakfast,
@@ -119,19 +122,6 @@ def serialize(self):
             "lunch": self.lunch,
             "dinner": self.dinner,
             "supper": self.supper,
-            "id_trainer": self.get_trainer_serialized(),  # Serializar entrenador si existe
-            "id_user": self.get_user_serialized()  # Serializar usuario si existe
-        }
-
-
-
-
-    
-    
-    
-    
-    
-    
-    
-
-    
+            "id_trainer": self.get_trainer_serialized(),  
+            "id_user": self.get_user_serialized()  
+        }  
