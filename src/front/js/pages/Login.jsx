1abea213262d0b330e73122e4f98702f87ced85d
Alexander Gustavo Castillo <114032036+23ALX2227@users.noreply.github.com>
2023-12-23 03:06:24 +0000

    Create Login Action

--- src/front/js/pages/Login.jsx
+++ src/front/js/pages/Login.jsx
@@ -1,23 +1,40 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import { Context } from "../store/appContext";
+import { Link, useNavigate } from 'react-router-dom'
 import '../../styles/Login.module.css'
-import { Link } from 'react-router-dom'
+
 
 export const Login = () => {
+    const { store, actions } = useContext(Context);
+    const navigate = useNavigate();
+    const [userEmail, setUserEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const handleClick = () => {
+        actions.logIn(userEmail, password)        
+    }
+    
+    useEffect(() => {
+        if (store.loginRes == "Success") {
+            navigate('/user')            
+        }       
+
+    }, [store.loginRes.length])
+
     return (    
         <div className='container-fluid'>
             <div className='row'>
                 <div className='col-md-3 m-auto mt-5 p-5 border border-danger-subtle'>
-                    <form>
+                    
                         <div className="form-floating mb-3">
-                            <input type="email" className="form-control" id="email" placeholder="name@example.com" />
-                            <label htmlFor="email">Email address</label>
+                            <input type="email" className="form-control" id="email" placeholder="name@example.com" value={userEmail} onChange={(e)=>setUserEmail(e.target.value)} />
+                            <label>Email address</label>
                         </div>
                         <div className="form-floating mb-3">
-                            <input type="password" className="form-control" id="Password" placeholder="Password" />
-                            <label htmlFor="Password">Password</label>
+                            <input type="password" className="form-control" id="Password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)} />
+                            <label>Password</label>
                         </div>
-                        <button type="submit" className="btn btn-dark">Submit</button>
-                    </form>
+                        <button className="btn btn-dark" onClick={()=> handleClick()}>Submit</button>
+                    
                     <div className='m-3'>
                         <Link className='text-danger' to="/signup">Enter here to register!</Link>
                     </div>
