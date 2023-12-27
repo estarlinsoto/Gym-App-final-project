d1ce54c8b9f3da8b5b05106bcb51e5b0099d06ee
chrisgonvill <124818047+chrisgonvill@users.noreply.github.com>
2023-12-27 21:37:24 +0000

    tTesting video clips

--- src/views/User.jsx
+++ src/views/User.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import style from '../../src/front/styles/User.module.css'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 
 const User = () => {
     return (
 
-        
+
         <div>
-            
-            
+
+
 
             <div className={style.main}>
                 <div className={style.main__container}>
@@ -17,36 +17,39 @@ const User = () => {
                         <h1>Get In Action Now</h1>
                         <h2></h2>
                         <p>Checkout your daily routines and diet.</p>
-                        
+
                     </div>
                     <div className={style.main__img__container}>
-                        <img src="https://images.pexels.com/photos/4162583/pexels-photo-4162583.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="pic" id={style.main__img} />
+                        <video controls autoPlay className={style.main__video__container}>
+                            <source src="https://player.vimeo.com/external/456109483.sd.mp4?s=d7a63dff668ee01dc30fff844811fc751df40e5f&profile_id=165&oauth2_token_id=57447761" type="video/mp4" />
+                            Tu navegador no admite el elemento de video.
+                        </video>
                     </div>
+
                 </div>
             </div>
 
 
             <div className={style.services}>
-                <h1>Get The Best Results</h1>
+                <h1>Get Your Best Results Now</h1>
                 <div className={style.services__container}>
-                    
                     <div className={style.services__card}>
-                    
-                        <h2>Your Routines</h2>
-                        <p></p>
-                        
+                        <Link to="/routines">
+                            <h2>Your Routines</h2>
+                            <p></p>
+                        </Link>
                     </div>
                     <div className={style.services__card}>
-                    
-                        <h2>Your Diets</h2>
-                        <p></p>
-                        
+                        <Link to="/diets">
+                            <h2>Your Diets</h2>
+                            <p></p>
+                        </Link>
                     </div>
                     <div className={style.services__card}>
-                    
-                        <h2>Your Progress</h2>
-                        <p></p>
-                        
+                        <Link to="/progress">
+                            <h2>Your Progress</h2>
+                            <p></p>
+                        </Link>
                     </div>
                 </div>
             </div>
@@ -95,24 +98,24 @@ const User = () => {
                             <a href="/" id={style.footer__logo}><i className="fa-solid fa-dumbbell"></i>__GYMApp</a>
                         </div>
                         <p className={style.website__rights}><i class="fa-solid fa-copyright"></i>GYMApp 2023. All rights reserved</p>
-                    <div className={style.social__icons}>
-                        <a href="/" className={style.social__icon__link} target="_blank">
-                            <i className="fa-brands fa-facebook"></i>
-                        </a>
-                        <a href="/" className={style.social__icon__link} target="_blank">
-                            <i className="fa-brands fa-instagram"></i>
-                        </a>
-                        <a href="/" className={style.social__icon__link} target="_blank">
-                            <i className="fa-brands fa-youtube"></i>
-                        </a>
-                        <a href="/" className={style.social__icon__link} target="_blank">
-                        <i class="fa-brands fa-linkedin"></i>
-                        </a>
+                        <div className={style.social__icons}>
+                            <a href="/" className={style.social__icon__link} target="_blank">
+                                <i className="fa-brands fa-facebook"></i>
+                            </a>
+                            <a href="/" className={style.social__icon__link} target="_blank">
+                                <i className="fa-brands fa-instagram"></i>
+                            </a>
+                            <a href="/" className={style.social__icon__link} target="_blank">
+                                <i className="fa-brands fa-youtube"></i>
+                            </a>
+                            <a href="/" className={style.social__icon__link} target="_blank">
+                                <i class="fa-brands fa-linkedin"></i>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div >
+        </div >
     )
 }
 
