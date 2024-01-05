254f95fbfb7781892506c99866f849e660f52aef
chrisgonvill <124818047+chrisgonvill@users.noreply.github.com>
2024-01-05 03:57:44 +0000

    Added styles to login view

--- src/front/js/pages/Login.jsx
+++ src/front/js/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState, useEffect } from 'react'
 import { Context } from "../store/appContext";
 import { Link, useNavigate } from 'react-router-dom'
+import style from "../../styles/Login.module.css"
 
 
 export const Login = () => {
@@ -9,34 +10,39 @@ export const Login = () => {
     const [userEmail, setUserEmail] = useState("")
     const [password, setPassword] = useState("")
 
-    
+
     const handleClick = () => {
-        actions.logIn(userEmail, password)        
+        actions.logIn(userEmail, password)
     }
-    
+
     useEffect(() => {
         if (store.loginRes == "Success") {
-            navigate('/user')            
+            navigate('/user')
         }
     }, [store.loginRes])
 
-    return (    
-        <div className='container-fluid'>
-            <div className='row'>
-                <div className='col-md-3 m-auto mt-5 p-5 border border-danger-subtle'>
-                    
-                        <div className="form-floating mb-3">
-                            <input type="email" className="form-control" id="email" placeholder="name@example.com" value={userEmail} onChange={(e)=>setUserEmail(e.target.value)} />
-                            <label>Email address</label>
-                        </div>
-                        <div className="form-floating mb-3">
-                            <input type="password" className="form-control" id="Password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)} />
-                            <label>Password</label>
-                        </div>
-                        <button className="btn btn-dark" onClick={()=> handleClick()}>Submit</button>
-                    
-                    <div className='m-3'>
-                        <Link className='text-danger' to="/signup">Enter here to register!</Link>
+    return (
+        <div className={style.container}>
+            <div>
+                <div className={style.logo_box}>
+                <Link to='/' className={style.logo_login}><i class="fa-solid fa-dumbbell">_GYMApp</i></Link>
+                </div>
+                <div className={style.welcome}>
+                    <p>Welcome to your best workout</p>
+                </div>
+                <div className={style.form_container}>
+                    <div className={style.form_floating}>
+                        <input type="email" className={style.form_control} id="email" placeholder="example@gmail.com" value={userEmail} onChange={(e) => setUserEmail(e.target.value)} />
+                        <label>Email address</label>
+                    </div>
+                    <div className={style.form_floating}>
+                        <input type="password" className={style.form_control} id="Password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        <label>Password</label>
+                    </div>
+                    <button className={style.btn} onClick={() => handleClick()}>Submit</button>
+
+                    <div className={style.m_3}>
+                        <Link className={style.text_danger} to="/signup">Not registered?</Link>
                     </div>
                 </div>
             </div>
