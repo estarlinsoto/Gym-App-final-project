f36f34b86de89a45af139df5dce0f162afd1b84b
chrisgonvill <124818047+chrisgonvill@users.noreply.github.com>
2024-01-05 21:33:49 +0000

    Added details to the Admin View

--- src/views/Admin.jsx
+++ src/views/Admin.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import style from '../../src/front/styles/Admin.module.css'
-import { Navbar_Admin } from '../front/js/component/Navbar_Admin.jsx'
+
 import { Link } from 'react-router-dom'
 
 
@@ -11,29 +11,18 @@ const Admin = () => {
 
             <admin className="">
                 <div className={style.services}>
-                    <h1>See what the hype is about</h1>
+                    <h1>Administrators can manage Users, Trainers and Finances from here.</h1>
                     <div className={style.services__container}>
-                        <Link to='/'>
-                            <div className={style.services__card}>
-                                <h2>Experience Fitness</h2>
-                                <p>Best Technology Gym</p>
-                                {/* <button>Get Started</button> */}
-                            </div>
+                        <Link to='/admin/users' className={style.services__card}>
+                            <h2>Manage your Users</h2>
                         </Link>
-                        <div className={style.services__card}>
-
-                            <h2>Are You Ready</h2>
-                            <p>Take The Challenge</p>
-                            {/* <button>Get Started</button> */}
-                        </div>
-                        <div className={style.services__card}>
-
-                            <h2>Transform Yourself</h2>
-                            <p>Get Motivated</p>
-                            {/* <button>Get Started</button> */}
-                        </div>
+                        <Link to='/admin/trainers' className={style.services__card}>
+                            <h2>Manage your Trainers</h2>
+                        </Link>
+                        <Link to='/admin/finances' className={style.services__card} Manage your Finances >
+                            <h2>Manage your Finances</h2>
+                        </Link>
                     </div>
-                    
                 </div>
             </admin>
         </div>
