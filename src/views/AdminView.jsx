0a38f29a1863f58dc603dec93c1304bb3b0aa123
Alexander Gustavo Castillo <114032036+23ALX2227@users.noreply.github.com>
2024-01-09 00:48:54 +0000

    Admin View Terminado

---
+++ src/views/AdminView.jsx
@@ -0,0 +1,48 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Context } from "../front/js/store/appContext.js";
+import { Link, Routes, Route, useNavigate } from "react-router-dom"
+import style from '../../src/front/styles/User.module.css'
+
+
+export const AdminView = () => {
+    const { store, actions } = useContext(Context);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        actions.privateViewRequest()
+        if(store.privateRes === true || sessionStorage.access_token == ""){
+            navigate('/')
+        }
+
+    }, [store.privateRes])
+
+
+
+    return (
+        
+            <div className={style.services}>
+            <h1>Admin</h1>
+            <div className={style.services__container}>
+                <div className={style.services__card}>
+                    <Link to="/admin/users">
+                        <h2>Your Clients</h2>
+                        <p>Here you can delete users</p>
+                    </Link>
+                </div>
+                <div className={style.services__card}>
+                    <Link to="/admin/trainer">
+                        <h2>Your Trainers</h2>
+                        <p>Here you can delete users</p>
+                    </Link>
+                </div>
+                <div className={style.services__card}>
+                    <Link to="/user/progress">
+                        <h2>Your Progress</h2>
+                        <p></p>
+                    </Link>
+                </div>
+            </div>
+        </div>
+         
+    );
+};
